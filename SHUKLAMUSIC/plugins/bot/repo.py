4e86afd51c4c06c41ad1d9d4657f79ac78a0cbb8
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-11-25 14:15:56 -1200

    ─ ❖ | ʀɪsʜυ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖ ─

---
+++ SHUKLAMUSIC/plugins/bot/repo.py
@@ -0,0 +1,69 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from SHUKLAMUSIC import app
+from config import BOT_USERNAME
+from SHUKLAMUSIC.utils.errors import capture_err
+import httpx 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+start_txt = """
+➤ ᴅᴇᴘʟᴏʏ ᴇᴀsɪʟʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴇʀʀᴏʀꜱ  
+
+┏━━━━━━━━━━━━━━━━━⧫
+┠ ◆ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: [Click Here](https://t.me/oyekanhaa)  
+┠ ◆ ᴅᴇᴠᴇʟᴏᴘᴇʀ: [𝐊 𝐀 𝐍 𝐇 Λ](https://t.me/oyekanhaa)
+┠ ◆ ʀᴇʟᴇᴀsᴇᴅ ʙʏ: [Aboutkanha](https://t.me/aboutkanha)
+┗━━━━━━━━━━━━━━━━━⧫
+
+ɪꜰ ʏᴏᴜ ꜰᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜱꜱ ɪɴ ꜱᴜᴘᴘᴏʀᴛ
+"""
+
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [
+                InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧", url="https://t.me/kanhasworld"),
+                InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘", url="https://t.me/aboutkanha")
+        ],
+        [ 
+          InlineKeyboardButton("𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘", url=f"https://github.com/TeamProBots/PurviBots")
+        ]
+    ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://i.ibb.co/gFm6VW52/source-code.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+ 
+   
+# --------------
+
+
+@app.on_message(filters.command("repo", prefixes="#"))
+@capture_err
+async def repo(_, message):
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://api.github.com/repos/TeamProBots/PurviBots/contributors")
+    
+    if response.status_code == 200:
+        users = response.json()
+        list_of_users = ""
+        count = 1
+        for user in users:
+            list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
+            count += 1
+
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://t.me/ProBotts) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/ProBotGc)
+| 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
+----------------
+{list_of_users}"""
+        await app.send_message(message.chat.id, text=text, disable_web_page_preview=True)
+    else:
+        await app.send_message(message.chat.id, text="Failed to fetch contributors.")
